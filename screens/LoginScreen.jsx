e4083ee27cb9ea60bc9725798d8593f57a39767e
Bruno Dimon <Bruno_gxis@outlook.com>
2022-11-30 20:01:08 -0300

    começado a alterar para dark e light mode

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { View, Image } from "react-native";
 
-import { Icon, Pressable, Center } from "native-base";
+import { Icon, Pressable, Center, Switch, useColorMode, useColorModeValue } from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 import { InputField } from "../components/InputField";
 
@@ -10,7 +10,7 @@ import { ButtonUnderline } from "../components/ButtonUnderline";
 
 import { AuthContext } from "../navigation/AuthContext"
 
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 
@@ -20,13 +20,39 @@ export function LoginScreen({ navigation }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const { colorMode, toggleColorMode } = useColorMode();
+  const themeSave = async themeData => {
+    try {
+      const jsonValue = JSON.stringify(themeData);
+      await AsyncStorage.setItem("@theme", jsonValue);
+      return true;
+    } catch (err) { }
+
+    return false;
+  };
+
+  const getUsuario = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("@usuario");
+      if (jsonValue !== null) {
+        const usuarioRecuperado = JSON.parse(jsonValue);
+        return usuarioRecuperado;
+      }
+    } catch (e) { }
+    return {
+      nome: "",
+      email: "",
+    };
+  };
+
   return (
-    <Center flex={1} bgColor={'gray.200'}>
-      <Image style={{ width: "90%", height: "60%" }} source={require("../assets/logofontepreta.png")}></Image>
+    <Center flex={1} _light={{ bgColor: 'gray.200' }} _dark={{ bgColor: 'dark.50' }}>
+      {colorMode == 'dark' ? <Image style={{ width: "90%", height: "60%" }} source={require("../assets/logofontebranca.png")} /> : <Image style={{ width: "90%", height: "60%" }} source={require("../assets/logofontepreta.png")} />}
+
 
       <InputField onChangeText={(valueEmail) => setEmail(valueEmail)} w={{ base: "85%", md: "15%" }} mb={2} InputLeftElement={<Icon as={<MaterialIcons name="person" />} size={5} ml="2" color="muted.400" />} placeholder="E-mail" />
       <InputField onChangeText={(valuePassword) => setPassword(valuePassword)} w={{ base: "85%", md: "15%" }} mb={2} type={show ? "text" : "password"} InputLeftElement={<Icon as={<MaterialIcons name="lock" />} size={5} ml="2" color="muted.400" />} InputRightElement={<Pressable onPress={() => setShow(!show)}><Icon as={<MaterialIcons name={show ? "visibility" : "visibility-off"} />} size={5} mr="2" color="muted.400" /></Pressable>} placeholder="Senha" />
-
+      <Switch offTrackColor="dark.100" onTrackColor="light.200" onThumbColor="dark.500" offThumbColor="light.300" onChange={toggleColorMode} />
       <ButtonContained title="Entrar" onPress={login} w={'30%'} />
       <ButtonUnderline title="Vizualicação Livros" onPress={() => navigation.navigate("Visualização de Livros")} mt={2} />
     </Center>
